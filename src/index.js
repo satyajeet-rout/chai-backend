58602b356ea8d0de571f393a4dd053f4fa55a170
Satyajeet Rout <satyajeetrout98@gmail.com>
2024-04-23 22:28:31 +0530

    add user and video model and etc...

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR: ", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at PORT : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!", err);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn)=>{()=>{}}    both are same
+// const asyncHandler = (fn) => () => {}  both are same
+// const asyncHandler = (fn) => async() => {}  both are same
+
+
+
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
